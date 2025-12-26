a0e767c6ca7f6fd0ce7d67e99d7220d41fc1eb3f
SonaliT712 <sonalitate712@gmail.com>
2025-12-26 03:37:22 -0800

    Delete attendance.py

--- attendance.py
+++
@@ -1,8 +0,0 @@
-# attendance.py
-
-def mark_attendance(student, present_days, total_days):
-    attendance_percentage = (present_days / total_days) * 100
-    student['attendance'] = attendance_percentage
-
-def get_attendance(student):
-    return student.get('attendance', 0)
